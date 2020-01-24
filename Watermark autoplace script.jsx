d796d96a4bb7eb60348ccf21ab8fd51ed95444dd
apedreira <52757793+apedreira@users.noreply.github.com>
2020-01-24 13:50:24 +0100

    Add files via upload
    A simple script to resize a watermark according to canvas width. 
    Combined with an action, provides you a full-automatic way to add watermarks to your photos.

---
+++ Watermark autoplace script.jsx
@@ -0,0 +1,32 @@
+﻿//Setting rule units
+    const defaultRulerUnits = app.preferences.rulerUnits;  
+           app.preferences.rulerUnits = Units.PIXELS;
+    
+//Setting canvas and watermark (active layer) constiables
+    const doc = app.activeDocument;  
+    const layer = doc.activeLayer;  
+    const width = doc.width.as('px');  
+    const height =doc.height.as('px');  
+    const bounds = app.activeDocument.activeLayer.bounds;  
+    const layerWidth = bounds[2].as('px')-bounds[0].as('px');  
+    const layerHeight = bounds[3].as('px')-bounds[1].as('px');  
+    const layerRatio = layerWidth / layerHeight;  
+           const newWidth = width;  
+           const newHeight = ((1.0 * width) / layerRatio); 
+
+//Resizing watermark (active layer) acording to canvas size
+  const resizeWidthPercent = newWidth/layerWidth*35;  
+  const resizeHeightPercent = newHeight/layerHeight*35;
+        app.activeDocument.activeLayer.resize(resizeWidthPercent,resizeHeightPercent,AnchorPosition.BOTTOMLEFT);  
+        
+//Setting watermark (active layer) opacity
+  const Opacity = 69; 
+         activeDocument.activeLayer.opacity=Opacity;
+       
+//Slightly separating watermark from canvas edges
+   const OffsetX = 100; 
+   const OffsetY = -100; 
+         layer.translate (OffsetX, OffsetY);
+    
+//Setting default unit tules
+app.preferences.rulerUnits = defaultRulerUnits;  
