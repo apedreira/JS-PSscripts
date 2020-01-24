c4a4a653f92a64b49aec4110619882295b4dc55a
apedreira <52757793+apedreira@users.noreply.github.com>
2020-01-24 13:58:12 +0100

    Add files via upload
    A simple script for resize text layer to fit in a desired width

---
+++ textResize.jsx
@@ -0,0 +1,20 @@
+﻿          
+const layer = activeDocument.activeLayer; //Grab the currently selected layer
+    
+// Calculate length and width based on the rectangular bounds of the selected layer
+const oriHeight = layer.bounds[3].value - layer.bounds[1].value; //Grab the original width
+const oriWidth = layer.bounds[2].value - layer.bounds[0].value; //Grab the original width
+const desWidth = 2240; //Desired width
+const diffWidth = oriWidth - desWidth; //Difference to rest 
+
+const per  = (desWidth *100) /oriWidth;
+
+if(oriWidth > desWidth) {
+layer.resize(per,100, AnchorPosition.BOTTOMCENTER);
+
+
+}
+
+const newHeight =  layer.bounds[3].value - layer.bounds[1].value;
+
+const heightReduction=  newHeight*100/oriHeight;
